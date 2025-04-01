254493caeda8d12d5128977a541c3a960d69a750
AlexHarbaugh1 <amh0053@uah.edu>
2025-03-31 19:47:03 -0500

    Fix GUI499.py
    This should fix the GUI back to when it wasworking

--- GUI499.py
+++ GUI499.py
@@ -1,10 +1,112 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLabel, QTabWidget, QWidget, QFormLayout, QApplication
-from PyQt5.QtCore import QObject, QTimer, QEvent
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 09:18:06 2025
+
+@author: laure
+"""
+
+import psycopg2
+import sys
+import pandas as pd
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-import sys
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QTableWidgetItem, QTabWidget, QVBoxLayout, QPushButton, QLabel, QFormLayout
+from PyQt5.QtCore import QTimer, QEvent, QObject
+import string
 import EncryptionKey
 import SearchDB
+
+class MainScreen(QDialog):
+    def __init__(self):
+        super(MainScreen, self).__init__()
+        loadUi("MainScreen.ui", self)
+        self.enterApplication.clicked.connect(self.openLogin)
+
+    def openLogin(self):
+        login = LoginScreen()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class LoginScreen(QDialog):
+    def __init__(self):
+        super(LoginScreen, self).__init__()
+        loadUi("login1.ui", self)
+        self.passwordField.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.login.clicked.connect(self.loginfunction)
+
+    def loginfunction(self):
+        user = self.userField.text()
+        password = self.passwordField.text()
+
+        if len(user) == 0 or len(password) == 0:
+            self.errorMsg.setText("Missing field.")
+        else:
+            keys = EncryptionKey.getKeys()
+            encryption_key = keys[0]
+            fixed_salt = keys[1]
+            result_pass = SearchDB.passwordMatch(user, password, fixed_salt)
+            if result_pass:
+                self.errorMsg.setText("")
+                self.gotosearch()
+            else:
+                self.errorMsg.setText("Invalid username or password")
+
+    def gotosearch(self):
+        search = SearchScreen()
+        widget.addWidget(search)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class SearchScreen(QDialog):
+    def __init__(self):
+        super(SearchScreen, self).__init__()
+        loadUi("patientsearch.ui", self)
+        self.search.clicked.connect(self.searchfunction)
+        self.logout.clicked.connect(LogOut)
+        self.resultsTable.hide()
+
+    def searchfunction(self):
+        lastName = self.lastField.text()
+        firstName = self.firstField.text()
+
+        if len(lastName) == 0 and len(firstName) == 0:
+            self.error.setText("Input at least one field.")
+        else:
+            keys = EncryptionKey.getKeys()
+            encryption_key = keys[0]
+            fixed_salt = keys[1]
+            df = None
+            if firstName and lastName:
+                df = pd.DataFrame(SearchDB.searchPatientWithName(firstName, None, lastName, encryption_key, fixed_salt, True))
+            else:
+                df = pd.DataFrame(SearchDB.searchPatientWithName(firstName, None, None, encryption_key, fixed_salt, True))
+                if df.empty:
+                    df = pd.DataFrame(SearchDB.searchPatientWithName(None, None, lastName, encryption_key, fixed_salt, True))
+
+            if df.empty:
+                self.error.setText("No results found.")
+            else:
+                self.error.setText("")
+                self.resultsTable.show()
+                self.resultsTable.setRowCount(len(df))
+                self.resultsTable.setColumnCount(len(df.columns))
+                self.resultsTable.setHorizontalHeaderLabels(["ID", "First Name", "Middle Name", "Last Name"])
+                for i in range(len(df)):
+                    for j in range(len(df.columns)):
+                        item = QTableWidgetItem(str(df.iat[i, j]))
+                        self.resultsTable.setItem(i, j, item)
+                self.resultsTable.cellDoubleClicked.connect(self.openPatientDetails)
+                self.df = df
+
+    def openPatientDetails(self, row, column):
+        patient_id = str(self.df.iat[row, 0])
+        details = PatientDetailsScreen(patient_id)
+        widget.addWidget(details)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotolist(self):
+        plist = ListScreen()
+        widget.addWidget(plist)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 class PatientDetailsScreen(QDialog):
     def __init__(self, patient_id):
@@ -40,6 +142,7 @@ class PatientDetailsScreen(QDialog):
         layout.addWidget(self.print_button)
 
         self.setLayout(layout)
+
         self.loadPatientData(patient_id)
 
     def loadPatientData(self, patient_id):
@@ -55,12 +158,12 @@ class PatientDetailsScreen(QDialog):
             info_layout.setSpacing(10)
             info_layout.setContentsMargins(20, 20, 20, 20)
             info_layout.addRow(QLabel("<b>Patient Information</b>"))
-            info_layout.addRow("First Name:", QLabel(str(patientData[0])))
-            info_layout.addRow("Middle Name:", QLabel(str(patientData[1])))
-            info_layout.addRow("Last Name:", QLabel(str(patientData[2])))
-            info_layout.addRow("Mailing Address:", QLabel(str(patientData[3])))
+            info_layout.addRow("First Name:", QLabel(patientData[0]))
+            info_layout.addRow("Middle Name:", QLabel(patientData[1]))
+            info_layout.addRow("Last Name:", QLabel(patientData[2]))
+            info_layout.addRow("Mailing Address:", QLabel(patientData[3]))
             info_layout.addRow(QLabel("<b>Doctor Info</b>"))
-            info_layout.addRow("Doctor Username:", QLabel(str(doctorUsername[0])))
+            info_layout.addRow("Doctor Username:", QLabel(doctorUsername[0]))
             info_layout.addRow("Doctor Name:", QLabel(f"{doctorUsername[1]} {doctorUsername[2]}"))
             if locationInfo:
                 info_layout.addRow(QLabel("<b>Room Info</b>"))
@@ -170,42 +273,63 @@ class PatientDetailsScreen(QDialog):
         current_widget = widget.widget(current_index)
         widget.removeWidget(current_widget)
         current_widget.deleteLater()
-        search_screen = QDialog()
+        search_screen = SearchScreen()
         widget.addWidget(search_screen)
         widget.setCurrentIndex(widget.count() - 1)
 
-class InactivityMonitor(QObject):
-    def __init__(self, timeout=30000, callback=None, parent=None):
-        super().__init__(parent)
-        self.callback = callback
-        self.timer = QTimer(self)
-        self.timer.setInterval(timeout)
-        self.timer.timeout.connect(self.handleTimeout)
-        self.timer.start()
+class ListScreen(QDialog):
+    def __init__(self):
+        super(ListScreen, self).__init__()
+        loadUi("list.ui", self)
 
-    def handleTimeout(self):
-        if self.callback:
-            self.callback()
-
-    def eventFilter(self, obj, event):
-        if event.type() in (QEvent.MouseMove, QEvent.KeyPress, QEvent.MouseButtonPress, QEvent.Wheel):
-            print("User activity detected. Resetting timer.")
-            self.timer.start()
-        return False
 
 def LogOut():
-    print("User inactive. Logging out...")
-    while widget.count() > 1:
-        widget.removeWidget(widget.widget(1))
-    widget.setCurrentIndex(0)
+    home = MainScreen()
+    widget.addWidget(home)
+    widget.setCurrentIndex(widget.currentIndex() + 1)
 
 app = QApplication(sys.argv)
-monitor = InactivityMonitor(timeout=30000, callback=LogOut)
-app.installEventFilter(monitor)
+app.setStyleSheet("""
+    QWidget {
+        font-size: 16px;
+        font-family: Arial, sans-serif;
+    }
+    QLabel {
+        padding: 2px;
+    }
+    QPushButton {
+        padding: 6px 12px;
+        font-weight: bold;
+        background-color: #e0e0e0;
+        border: 1px solid #aaa;
+        border-radius: 4px;
+    }
+    QPushButton:hover {
+        background-color: #d6d6d6;
+    }
+    QTableWidget {
+        gridline-color: #ccc;
+        font-size: 15px;
+    }
+    QTabWidget::pane {
+        border: 1px solid #ccc;
+    }
+    QTabBar::tab {
+        padding: 6px;
+        margin: 2px;
+        border: 1px solid #aaa;
+        border-radius: 4px;
+        background: #f0f0f0;
+    }
+    QTabBar::tab:selected {
+        background: #dcdcdc;
+        font-weight: bold;
+    }
+""")
+login = LoginScreen()
+home = MainScreen()
 widget = QtWidgets.QStackedWidget()
-login_screen = QDialog()
-loadUi("login1.ui", login_screen)
-widget.addWidget(login_screen)
+widget.addWidget(home)
 widget.resize(1200, 800)
 widget.show()
 try:
